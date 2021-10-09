8355b30de9b78441efa89e756e8e13cc889d29da
dragonappear <dragonappear1@gmail.com>
2021-10-09 23:24:36 +0900

    유저포인트 서비스 및 테스트 코드 개발

--- src/main/java/com/dragonappear/inha/service/user/UserPointService.java
+++ src/main/java/com/dragonappear/inha/service/user/UserPointService.java
@@ -1,7 +1,10 @@
 package com.dragonappear.inha.service.user;
 
+import com.dragonappear.inha.domain.user.User;
 import com.dragonappear.inha.domain.user.UserPoint;
+import com.dragonappear.inha.domain.value.Money;
 import com.dragonappear.inha.repository.user.UserPointRepository;
+import com.dragonappear.inha.repository.user.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -11,6 +14,37 @@ import org.springframework.transaction.annotation.Transactional;
 @Service
 public class UserPointService {
     private final UserPointRepository userPointRepository;
+    private final UserRepository userRepository;
+
+    // 포인트 생성
+    public Long create(Long userId) {
+        User findUser = userRepository.findById(userId).orElseThrow(() -> new IllegalStateException("존재하지 않는 회원입니다"));
+        UserPoint newPoint = new UserPoint(findUser);
+        return userPointRepository.save(newPoint).getId();
+    }
+
+    //  포인트 조회 by 포인트 아이디로
+    public UserPoint findByPointId(Long userPointId) {
+        return userPointRepository.findById(userPointId).orElseThrow(() -> new IllegalStateException("존재하지 않는 포인트입니다"));
+    }
+
+    // 포인트 조회 by 유저 아이디로
+    public UserPoint findByUserId(Long userId) {
+        return userPointRepository.findByUserId(userId).orElseThrow(() -> new IllegalStateException("존재하지 않는 포인트입니다"));
+    }
+
+
+    // 포인트 적립
+    public UserPoint accumulate(UserPoint userPoint, Money amount) throws Exception {
+        userPoint.plus(amount.getAmount());
+        return userPoint;
+    }
+
+    //  포인트 차감
+    public UserPoint subtract(UserPoint userPoint, Money amount) throws Exception {
+        userPoint.minus(amount.getAmount());
+        return userPoint;
+    }
 
 
 }
