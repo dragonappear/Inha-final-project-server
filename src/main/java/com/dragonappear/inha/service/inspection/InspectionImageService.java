26b7dc954545af278cac00f3849b82f126600906
dragonappear <dragonappear1@gmail.com>
2021-10-12 02:05:53 +0900

    검수이미지 등록, 테스트코드 작성

--- src/main/java/com/dragonappear/inha/service/inspection/InspectionImageService.java
+++ src/main/java/com/dragonappear/inha/service/inspection/InspectionImageService.java
@@ -1,10 +1,13 @@
 package com.dragonappear.inha.service.inspection;
 
+import com.dragonappear.inha.domain.inspection.InspectionImage;
 import com.dragonappear.inha.repository.inspection.InspectionImageRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
 
 @RequiredArgsConstructor
 @Transactional(readOnly = true)
@@ -12,5 +15,14 @@ import org.springframework.transaction.annotation.Transactional;
 public class InspectionImageService {
     @Autowired InspectionImageRepository inspectionImageRepository;
 
+    // 검수이미지 등록
+    @Transactional
+    public void save(List<InspectionImage> image) {
+        image.stream().forEach(image1 -> {inspectionImageRepository.save(image1);});
+    }
 
+    // 검수이미지리스트 조회 by 검수이미지아이디
+    public List<InspectionImage> findByInspectionId(Long inspectionId) {
+        return inspectionImageRepository.findByInspectionId(inspectionId);
+    }
 }
