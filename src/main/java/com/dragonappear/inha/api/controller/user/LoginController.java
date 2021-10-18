85d3456e27ecd9526193f0879958981be6b1b2ae
dragonappear <dragonappear1@gmail.com>
2021-10-19 00:35:20 +0900

    DB ddl 수정

---
+++ src/main/java/com/dragonappear/inha/api/controller/user/LoginController.java
@@ -0,0 +1,22 @@
+package com.dragonappear.inha.api.controller.user;
+
+
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@Api(tags = {"유저 로그인 API"})
+@RequiredArgsConstructor
+@RestController
+public class LoginController {
+
+/*
+    @ApiOperation(value = "유저 DB 조회", notes = "유저 정보를 에서 조회합니다.")
+    @GetMapping("/users/{email}")
+    public UserLoginDto validateSession(@PathVariable("email") String email) {
+
+    }*/
+}
