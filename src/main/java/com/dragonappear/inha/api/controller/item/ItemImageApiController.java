349ef7abe31496422ad14a76626bcc0f66843f14
dragonappear <dragonappear1@gmail.com>
2021-10-28 19:18:12 +0900

    아이템 이미지 컨트롤러 API

--- src/main/java/com/dragonappear/inha/api/controller/item/ItemImageApiController.java
+++ src/main/java/com/dragonappear/inha/api/controller/item/ItemImageApiController.java
@@ -1,7 +1,10 @@
 package com.dragonappear.inha.api.controller.item;
 
+import com.dragonappear.inha.domain.item.ItemImage;
+import com.dragonappear.inha.service.item.ItemImageService;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
+import lombok.RequiredArgsConstructor;
 import org.springframework.core.io.FileSystemResource;
 import org.springframework.core.io.Resource;
 import org.springframework.http.HttpHeaders;
@@ -15,15 +18,38 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.List;
 
 @Api(tags = {"아이템 이미지 조회 API"})
+@RequiredArgsConstructor
 @RestController
 public class ItemImageApiController {
+    private final ItemImageService itemImageService;
 
-    @ApiOperation(value = "아이템 이미지 조회 API", notes = "아이템 이미지 조회")
+    @ApiOperation(value = "아이템 대표 이미지 조회 API by 파일이름으로", notes = "아이템 이미지 조회")
     @GetMapping(value = "/items/images/{fileOriginName}")
-    public ResponseEntity<Resource> itemImages(@PathVariable("fileOriginName") String fileName) {
+    public ResponseEntity<Resource> getItemImageByName(@PathVariable("fileOriginName") String fileName) {
         String path = "/home/ec2-user/app/step1/Inha-final-project-server/src/main/resources/static/items/";
+        FileSystemResource resource = new FileSystemResource(path+fileName);
+        if (!resource.exists()) {
+            return new ResponseEntity<Resource>(HttpStatus.NOT_FOUND);
+        }
+        HttpHeaders header = new HttpHeaders();
+        Path filePath = null;
+        try {
+            filePath = Paths.get(path+fileName);
+            header.add("Content-Type", Files.probeContentType(filePath));
+        } catch (IOException e) {
+            return new ResponseEntity<Resource>(HttpStatus.NOT_FOUND);
+        }
+        return new ResponseEntity<Resource>(resource, header, HttpStatus.OK);
+    }
+
+    @ApiOperation(value = "아이템 대표 이미지 조회 API by 아이템아이디로", notes = "아이템 이미지 조회")
+    @GetMapping(value = "/items/images/find/{itemId}")
+    public ResponseEntity<Resource> getItemImageById(@PathVariable("fileOriginName") Long itemId) {
+        String path = "/home/ec2-user/app/step1/Inha-final-project-server/src/main/resources/static/items/";
+        String fileName = itemImageService.findByItemId(itemId).get(0).getItemImage().getFileName();
         FileSystemResource resource = new FileSystemResource(path+fileName);
         if (!resource.exists()) {
             return new ResponseEntity<Resource>(HttpStatus.NOT_FOUND);
