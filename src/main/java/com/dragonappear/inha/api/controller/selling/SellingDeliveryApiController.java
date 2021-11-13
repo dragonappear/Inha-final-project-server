a7c910ed305c87f9342c3750a2a0734f57cf4aa6
dragonappear <dragonappear1@gmail.com>
2021-11-13 18:18:25 +0900

    택배 등록 API 수정

--- src/main/java/com/dragonappear/inha/api/controller/selling/SellingDeliveryApiController.java
+++ src/main/java/com/dragonappear/inha/api/controller/selling/SellingDeliveryApiController.java
@@ -3,11 +3,13 @@ package com.dragonappear.inha.api.controller.selling;
 import com.dragonappear.inha.api.controller.selling.dto.SellingDeliveryDto;
 import com.dragonappear.inha.api.returndto.MessageDto;
 import com.dragonappear.inha.api.service.firebase.FcmSendService;
+import com.dragonappear.inha.domain.deal.Deal;
 import com.dragonappear.inha.domain.deal.value.DealStatus;
 import com.dragonappear.inha.domain.selling.Selling;
 import com.dragonappear.inha.domain.selling.SellingDelivery;
 import com.dragonappear.inha.domain.user.User;
 import com.dragonappear.inha.exception.NotFoundCustomException;
+import com.dragonappear.inha.service.deal.DealService;
 import com.dragonappear.inha.service.selling.SellingDeliveryService;
 import com.dragonappear.inha.service.selling.SellingService;
 import io.swagger.annotations.Api;
@@ -28,22 +30,26 @@ public class SellingDeliveryApiController {
     private final SellingDeliveryService sellingDeliveryService;
     private final SellingService sellingService;
     private final FcmSendService fcmSendService;
+    private final DealService dealService;
 
     @ApiOperation(value = "택배 배송 등록 API", notes = "택배 배송 등록")
     @PostMapping("/sellings/deliveries/new")
     public MessageDto postSellingDelivery(@RequestBody SellingDeliveryDto dto) {
         Selling selling = sellingService.findBySellingId(dto.getSellingId());
+        Deal deal = selling.getDeal();
         if (selling.getSellingDelivery() != null) {
             return MessageDto.builder()
                     .message(getMessage("isUploadedSuccess", false, "Status", "택배등록이 이미 등록되었습니다."))
                     .build();
         }
+
         Long deliveryId = sellingDeliveryService.save(selling, dto.getDelivery());
-        selling.getDeal().updateDealStatus(DealStatus.판매자발송완료);
-        User buyer = selling.getDeal().getBuying().getPayment().getUser();
-        String title = "판매자 아이템 발송완료 알림";
-        String body =  "판매자가 "+selling.getAuctionitem().getItem().getItemName() + "을 발송하였습니다.";
+        dealService.updateDealStatus(deal,DealStatus.판매자발송완료);
+
         try {
+            User buyer = deal.getBuying().getPayment().getUser();
+            String title = "판매자 아이템 발송완료 알림";
+            String body =  "판매자가 "+selling.getAuctionitem().getItem().getItemName() + "을 발송하였습니다.";
             fcmSendService.sendFCM(buyer, title, body);
         } catch (IOException e) {
             log.error("deliveryId:{} 발송완료 FCM 메시지가 전송되지 않았습니다.",deliveryId);
