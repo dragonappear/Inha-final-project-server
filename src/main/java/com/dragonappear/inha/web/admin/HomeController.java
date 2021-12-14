58a7166af4269718307294aec84a618ac3f031b0
dragonappear <dragonappear1@gmail.com>
2021-12-15 08:05:57 +0900

    템플릿 수정

--- src/main/java/com/dragonappear/inha/web/admin/HomeController.java
+++ src/main/java/com/dragonappear/inha/web/admin/HomeController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class HomeController {
 
-    @GetMapping(value="/")
+    @GetMapping(value="/web/admin")
     public String home(){
         return "home";
     }
